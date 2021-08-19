d3bc07d140067bd4001429272034ad8476635b6c
Martin Starkov <emailregisterlogin@gmail.com>
2021-08-20 01:37:19 +0300

    Code clean up

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,18 @@
 #include "opencv2/highgui.hpp"
 #include <opencv2/imgproc/imgproc.hpp> // cv::cvtColor
 
-#include "Timer.h"
+#include "timer.h"
 
-#include <unordered_set>
+#include <array>
 #include <iostream>
 #include <filesystem>
 #include <vector>
 #include <string>
 #include <CL/sycl.hpp>
 
-#include <stdio.h>
-#include <stdlib.h>
-
 // TODO: Figure out why hash for DHash and AHash is negative.
 
 #define LOG(x) { std::cout << x << std::endl; }
-
-const char* wndname = "Square Detection Demo";
 
 using Hash = std::uint64_t;
 using Path = std::filesystem::path;
@@ -73,6 +68,7 @@ Hash GetAHash(const cv::Mat& resized_image) {
     // Cycle through every row
     std::size_t sum{ 0 };
     std::size_t pixels{ static_cast<std::size_t>(resized_image.rows * resized_image.cols) };
+    // Find average of all pixel brightnesses
     for (int index = 0; index < pixels; ++index) {
         // Get pixel pointer for the row
         auto ptr = (std::uint8_t*)(resized_image.data + index);
@@ -148,21 +144,19 @@ cv::Mat GetImage(const Path& path) {
 
             return {};
         }
+        case FileType::UNKNOWN:
+            return {};
     }
     return {};
 }
 
-
 std::vector<Hash> GetHashes(const std::vector<Path>& files) {
     std::size_t file_count{ files.size() };
+    
     LOG("[Starting hashing of " << file_count << " files]");
 
     std::vector<Hash> hashes;
     hashes.resize(file_count, 0);
-
-    std::size_t counter{ 0 };
-    std::size_t file_fraction{ file_count / static_cast<std::size_t>(100) };
-    std::size_t loading{ 1 };
 
     for (auto i{ 0 }; i < file_count; ++i) {
         const auto& path{ files[i] };
@@ -173,15 +167,8 @@ std::vector<Hash> GetHashes(const std::vector<Path>& files) {
         auto thumbnail{ Resize(greyscale, 8, 8) };
         
         hashes[i] = GetAHash(thumbnail);
-
-        counter += 1;
-        if (counter >= file_fraction) {
-            LOG("[" << loading << "% of files hashed]");
-            loading += 1;
-            counter = 0;
-        }
-
     }
+    
     LOG("[Finished hashing all files]");
     return hashes;
 }
@@ -206,82 +193,46 @@ inline std::uint64_t GetHammingDistance(Hash hash1, Hash hash2) {
 }
 
 using Pair = std::uint8_t;
-using PairContainer = std::vector<std::uint8_t>;
+using PairContainer = std::vector<Pair>;
 
 PairContainer ProcessDuplicates(std::vector<Hash>& hashes, int hamming_threshold) {
     std::size_t hash_count{ hashes.size() };
     LOG("[Starting duplicate search through " << hash_count << " hashes]");
 
-    std::size_t counter{ 0 };
-    std::size_t file_fraction{ hashes.size() / static_cast<std::size_t>(100) };
-    std::size_t loading{ 1 };
-    cl::sycl::property_list properties{ cl::sycl::property::buffer::use_host_ptr {} };
-
-    cl::sycl::range<1> hash_range{ hash_count };
-
     std::size_t pair_count{ hash_count * (hash_count - 1) / 2 };
-
-    cl::sycl::range<1> destination_range{ pair_count };
     
     PairContainer pairs;
     pairs.resize(pair_count, 0);
-    {
-    cl::sycl::buffer<Hash, 1> hash_b{ hashes, properties };
-    cl::sycl::buffer<Pair, 1> destination_b{ pairs, properties };
-
+    
     cl::sycl::queue queue;
+    cl::sycl::range<1> destination_range{ pair_count };
+    cl::sycl::range<1> hash_range{ hash_count };
+    cl::sycl::property_list properties{ cl::sycl::property::buffer::use_host_ptr {} };
+    
+    {
+        cl::sycl::buffer<Hash, 1> hash_b{ hashes, properties };
+        cl::sycl::buffer<Pair, 1> destination_b{ pairs, properties };
 
-    LOG("PreHashes [i=4, j=1]: [" << hashes[4] << ", " << hashes[1] << "]");
-    auto debug_index = GetLinearIndex(hash_count, 4, 1);
-    LOG("PrePair is: " << debug_index << ":" << unsigned(pairs[debug_index]));
-    LOG("PreHamming: " << GetHammingDistance(hashes[4], hashes[1]));
+        queue.submit([&](cl::sycl::handler& cgh) {
+            auto destination_a = destination_b.template get_access<cl::sycl::access::mode::write>(cgh);
+            auto hash_a = hash_b.template get_access<cl::sycl::access::mode::read>(cgh);
 
-    queue.submit([&](cl::sycl::handler& cgh) {
-        auto destination_a = destination_b.template get_access<cl::sycl::access::mode::read_write>(cgh);
-        auto hash_a = hash_b.template get_access<cl::sycl::access::mode::read>(cgh);
-        auto os = sycl::stream{ 50000, 4000, cgh };
-        auto kernel = [=](cl::sycl::id<1> id) {
-            auto i{ id.get(0) };
-            for (auto j{ 0 }; j < i; ++j) {
-                auto index = GetLinearIndex(hash_count, i, j);
-                auto hamming_distance{ GetHammingDistance(hash_a[i], hash_a[j]) };
-                if (i == 4 && j == 1) {
-                    os << "DuringPreHashes [i=4, j=1]: [" << hash_a[i] << ", " << hash_a[j] << "]" << "\n";
-                    os << "DuringPrePair is: " << index << ":" << destination_a[index] << "\n";
-                    os << "DuringHamming: " << hamming_distance << "\n";
+            auto kernel = [=](cl::sycl::id<1> id) {
+                auto i{ id.get(0) };
+                for (auto j{ 0 }; j < i; ++j) {
+                    auto index = GetLinearIndex(hash_count, i, j);
+                    auto hamming_distance{ GetHammingDistance(hash_a[i], hash_a[j]) };
+                    if (hamming_distance <= hamming_threshold) {
+                        //int similarity{ static_cast<int>(1.0 - static_cast<double>(hamming_distance) / (static_cast<double>(hamming_threshold) + 1.0) * 100.0) };
+                        destination_a[index] = hamming_distance + 1;
+                    }
                 }
-                if (hamming_distance == 0) {
-                    destination_a[index] = 1;
-                    //int similarity{ static_cast<int>(1.0 - static_cast<double>(hamming_distance) / (static_cast<double>(hamming_threshold) + 1.0) * 100.0) };
-                    
-                }
-                if (i == 4 && j == 1) {
-                    os << "DuringPostHashes [i=4, j=1]: [" << hash_a[i] << ", " << hash_a[j] << "]" << "\n";
-                    os << "DuringPostPair is: " << index << ":" << destination_a[index] << "\n";
-                }
-            }
-        };
-        cgh.parallel_for<class find_similar_images>(hash_range, kernel);
-    });
-    queue.wait();
+            };
+
+            cgh.parallel_for<class find_similar_images>(hash_range, kernel);
+        });
+        queue.wait();
     }
-    //for (const auto& file1 : hashes) {
-    //    for (const auto& file2 : hashes) {
-    //        if (file1 != file2) {
-    //            auto hamming_distance{ GetHammingDistance(file1.hash, file2.hash) };
-    //            if (hamming_distance <= hamming_threshold) {
-    //                int similarity{ static_cast<int>(1.0 - static_cast<double>(hamming_distance) / (static_cast<double>(hamming_threshold) + 1.0) * 100.0) };
-    //                //pairs.emplace(std::pair<File, File>{ file1, file2 }, similarity);
-    //            }
-    //        }
-    //    }
-    //    counter += 1;
-    //    if (counter >= file_fraction) {
-    //        LOG("[" << loading << "% of duplicates processed]");
-    //        loading += 1;
-    //        counter = 0;
-    //    }
-    //}
     LOG("[Finished duplicate search]");
     return pairs;
 }
@@ -299,7 +250,7 @@ int main(int argc, char** argv) {
     for (auto i{ 0 }; i < hash_count; ++i) {
         for (auto j{ 0 }; j < i; ++j) {
             auto index = GetLinearIndex(hash_count, i, j);
-            if (pairs[index] == 1) {
+            if (pairs[index] > 0) {
                 auto h = GetHammingDistance(hashes[i], hashes[j]);
                 assert(i < files.size());
                 assert(j < files.size());
@@ -317,19 +268,4 @@ int main(int argc, char** argv) {
     }
 
     cv::destroyAllWindows();
-        
-    
-
-    //for (const auto& pair : pairs) {
-    //    std::array<cv::Mat, 2> images{
-    //        Resize(GetImage(pair.duplicates.first.path), window.width / 2, window.height),
-    //        Resize(GetImage(pair.duplicates.second.path), window.width / 2, window.height)
-    //    };
-    //    cv::Mat concatenated;
-    //    // Concatenate duplicates images into one big image
-    //    cv::hconcat(images.data(), 2, concatenated);
-    //    cv::imshow("Duplicate Finder", concatenated);
-    //    cv::waitKey(0);
-    //}
-
 }
